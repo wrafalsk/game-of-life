583c1b246a637d10008640c6a3359834dad4df4d
Jack <jwrafalski@gmail.com>
2025-07-02 16:22:00 -0400

    Add species behaviors in Petridish

--- src/main/java/com/example/gameoflife/Cell.java
+++ src/main/java/com/example/gameoflife/Cell.java
@@ -1,33 +1,50 @@
 package com.example.gameoflife;
 
 public class Cell {
-	private int alive;
-	private char [] shape = { '-', '@'};
+        public static final int SPECIES_NORMAL = 0;
+        public static final int SPECIES_COEXISTENT = 1;
+        public static final int SPECIES_AGGRESSIVE = 2;
+
+        private int alive;
+        private int species;
+        private char [] shape = { '-', '@', 'C', 'X'};
 	
 	
-	Cell(){
-		this.setAlive(0);
-	}
+        Cell(){
+                this.setAlive(0);
+                this.species = SPECIES_NORMAL;
+        }
 
 	public int getAlive() {
 		return alive;
 	}
 
-	public void setAlive(int alive) {
-		this.alive = alive;
-	}
+        public void setAlive(int alive) {
+                this.alive = alive;
+        }
+
+        public int getSpecies() {
+                return species;
+        }
+
+        public void setSpecies(int species) {
+                this.species = species;
+        }
 	
         public char getAliveShape() {
-                return shape[1];
+                return shape[species + 1];
         }
 
         public char getDeadShape() {
                 return shape[0];
         }
-	
-	public char getCurrentShape() {
-		return this.shape[this.alive];
-	}	
+
+        public char getCurrentShape() {
+                if (this.alive == 0) {
+                        return shape[0];
+                }
+                return this.shape[this.species + 1];
+        }
 	
 	public void makeAlive(){
 		this.alive = 1;
